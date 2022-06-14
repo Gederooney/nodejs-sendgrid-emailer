707ec2da6f76efb08ef7d8d77469b0774b57ee48
gederooney <gyebadokpo61@gmail.com>
2022-06-14 10:21:14 -0400

    cors

--- src/app.mjs
+++ src/app.mjs
@@ -6,11 +6,22 @@ import cors from "cors";
 
 const app = express();
 
-app.use(
-	cors({
-		origin: ["https://omnidev.ca/", "https://omnigo.ca/", "http://localhost:3000"],
-	})
-);
+const whitelist = [
+	"http://localhost:8080",
+	"https://omnidev.ca/",
+	"https://omnigo.ca/",
+];
+const corsOptions = {
+	origin: function (origin, callback) {
+		if (whitelist.indexOf(origin) !== -1 || !origin) {
+			callback(null, true);
+		} else {
+			callback(new Error("Not allowed by CORS"));
+		}
+	},
+};
+
+app.use(cors(corsOptions));
 app.use(urlencoded({ extended: true }));
 app.use(json());
 app.use("/", router);
