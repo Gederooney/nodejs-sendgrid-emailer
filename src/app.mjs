5576209e287d057cccfd213f3a41486dd94a5083
gederooney <gyebadokpo61@gmail.com>
2022-06-14 09:52:21 -0400

    cors

--- src/app.mjs
+++ src/app.mjs
@@ -2,9 +2,15 @@ import express, { urlencoded, json } from "express";
 import helmet from "helmet";
 import morgan from "morgan";
 import router from "./routes/index.mjs";
+import cors from "cors";
 
 const app = express();
 
+app.use(
+	cors({
+		origin: ["https://omnidev.ca/", "https://omnigo.ca/", "http://localhost:3000"],
+	})
+);
 app.use(urlencoded({ extended: true }));
 app.use(json());
 app.use("/", router);
